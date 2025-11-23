6e76cf06fad135306a98cc393ce317e6816a3f73
Dhanvin2406 <nandusp269@gmail.com>
2025-11-23 12:26:41 +0530

    edited main css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// StrictMode disabled to prevent socket connection spam in development
+// due to double-mounting behavior
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <App />
 );
